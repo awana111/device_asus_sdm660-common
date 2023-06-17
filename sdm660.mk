8fc47922d9c07d8adec55cbf4e5d89f06cb4a548
Vivekachooz <vivekachooz@gmail.com>
2023-06-17 21:17:09 +0530

    sdm660-common: configs: Add powerhint for X00TD
    Change-Id: I6d3177b65340fd6311292b373131fe1de66c2b93

--- sdm660.mk
+++ sdm660.mk
@@ -389,6 +389,10 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
+# Powerhint
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
