62f9e3f57d41a4257dd8e0018fb64666d96a7509
Michael Bestas <mkbestas@lineageos.org>
2024-07-20 21:55:31 +0530

    sdm660-common: Inherit from legacy QTI FCM
    Change-Id: I0bde1c273d4abe689f61e7d4ebcf80965d785489

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -171,6 +171,7 @@ LOC_HIDL_VERSION := 4.3
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     vendor/infinity/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
