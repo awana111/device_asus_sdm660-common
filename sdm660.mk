72ecc80eef17ee0c2347dcda0f72a360c3cafb5e
Bruno Martins <bgcngm@gmail.com>
2024-07-22 00:24:06 +0530

    sdm660-common: Use common libqti-perfd-client and power-libperfmgr
    Change-Id: I7cc2a5ec484b9e7e733131f41004a1646b661aa7

--- sdm660.mk
+++ sdm660.mk
@@ -382,15 +382,9 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0 \
-    android.hardware.power@1.0.vendor \
-    android.hardware.power@1.1 \
-    android.hardware.power@1.1.vendor \
-    android.hardware.power@1.2 \
-    android.hardware.power@1.2.vendor \
-    android.hardware.power@1.3 \
     android.hardware.power@1.3.vendor \
-    android.hardware.power-service-qti
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
 
 # Perf
 PRODUCT_PACKAGES += \
@@ -486,6 +480,10 @@ PRODUCT_COPY_FILES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client \
     vendor/qcom/opensource/usb/etc
 
 QCOM_SOONG_NAMESPACE := \
