4e316071442ff39cba20c65e132b81fb0890fa30
Kneba <abenkenary3@gmail.com>
2025-05-20 03:17:26 +0000

    sdm660-common: Drop QTI Power/Perf service
    Change-Id: I70e918de4f0720963b2fd08a4f16b73dbf8867b6

--- sdm660.mk
+++ sdm660.mk
@@ -394,12 +394,8 @@ PRODUCT_PACKAGES += \
 
 # Perf
 PRODUCT_PACKAGES += \
-   libtflite \
-   libtextclassifier_hash \
-   vendor.qti.hardware.perf@2.2.vendor
-    
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf    
+    libtflite \
+    libtextclassifier_hash
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
