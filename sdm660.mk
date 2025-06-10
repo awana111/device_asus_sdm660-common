33d15dd3680d9b405b337531f1812f2c0fe11a38
Tiktodz <dotkit@electrowizard.me>
2025-06-10 08:20:38 +0000

    sdm660-common: Build display config 1.1 from source
    Change-Id: I448dd589af6f64d1a3ed590c871582e92cacf225

--- sdm660.mk
+++ sdm660.mk
@@ -160,6 +160,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@4.0.vendor \
     vendor.qti.hardware.memtrack-service \
     vendor.display.config@1.0.vendor \
+    vendor.display.config@1.1.vendor \
     vendor.display.config@2.0
 
 PRODUCT_PACKAGES += \
