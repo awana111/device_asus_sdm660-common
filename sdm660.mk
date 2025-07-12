f0f6f399573461644ace1bb2538757679ef689d2
Kneba <abenkenary3@gmail.com>
2025-07-12 16:10:23 +0000

    sdm660-common: lineage => aosp

--- sdm660.mk
+++ sdm660.mk
@@ -329,8 +329,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(COMMON_PATH)/overlay \
-    $(COMMON_PATH)/overlay-translates \
-    $(COMMON_PATH)/overlay-lineage
+    $(COMMON_PATH)/overlay-aosp
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
